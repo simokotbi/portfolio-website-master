161e55d2ebbe983cd5cba92acbd5c583073d93ed
Kotbi Mohamed <simojetta2@gmail.com>
2021-04-20 19:51:33 -0700

    comited

--- portfolio-website-master/script.js
+++ portfolio-website-master/script.js
@@ -38,3 +38,32 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+//animations:
+const text=document.querySelector('.fancy');
+const strtext=text.textContent;
+
+const splitText=strtext.split("");
+// .replace(/\s/g, "")
+text.textContent="";
+for(let i =0;i<splitText.length;i++){
+text.innerHTML+='<span>'+splitText[i]+'</span>';
+}
+let char=0;
+let timer=setInterval(onTick,50);
+
+function onTick(){
+
+const span=text.querySelectorAll("span")[char];
+
+ span.classList.add('fade');
+char++
+if(char===splitText.length){
+	stopanimation();
+	return;
+}
+}
+
+stopanimation=()=>{
+	clearInterval(timer);
+	timer=null;
+}
